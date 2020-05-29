7c7d86c0a9b333639330e6241acb3af16839b58e
AdrianFlierl <flierl.adrian@gmail.com>
2020-05-29 15:45:16 +0200

    Add Firebase, Account, Registration
    Anonyme Benutzer, Datenbank, Storage, Firebase dependencies, Account, Registration, few Style features

--- app/src/main/java/com/kneipenrallye/kneipenrallye/MainActivity.kt
+++ app/src/main/java/com/kneipenrallye/kneipenrallye/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,17 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, barlist::class.java).apply {
-            putExtra(AlarmClock.EXTRA_MESSAGE, "message");
-        }
-        startActivity(intent);
-    }
 
 
-    fun openBarlist(view: View) {
-        val intent = Intent(this, barlist::class.java).apply {
-            putExtra(AlarmClock.EXTRA_MESSAGE, "message");
+        btn_account.setOnClickListener {
+            val intent = Intent(this, Account::class.java).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "message");
+            }
+            startActivity(intent);
         }
-        startActivity(intent);
+
+
+        btn_main_reg.setOnClickListener {
+            val intent = Intent(this, Registration::class.java).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "message");
+            }
+            startActivity(intent);
+        }
+
+
+        btn_barlist.setOnClickListener {
+            val intent = Intent(this, barlist::class.java).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "message");
+            }
+            startActivity(intent);
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/kneipenrallye/kneipenrallye/Registration.kt
@@ -0,0 +1,65 @@
+package com.kneipenrallye.kneipenrallye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_registration.*
+
+class Registration : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+
+        // TODO: Auto generate random funny username
+
+        val spinner: Spinner = findViewById(R.id.spin_faculties)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.faculties_de,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+        spinner.setSelection(5)
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        updateUI(currentUser)
+//
+//
+//        // Firebase authentication
+//        auth = Firebase.auth
+//        FirebaseAuth.getInstance().createUserWithEmailAndPassword("kneipentash@trash-mail.com", "secure")
+//            .addOnCompleteListener()
+
+        btn_main_registration.setOnClickListener {
+
+            val posFaculti = spin_faculties.selectedItemPosition
+            val username = txt_username.text.toString()
+            Log.d("RegistrationActivity", "Username is:" + username)
+
+            if(username.isEmpty())
+            {
+                Toast.makeText(this, "Please enter Username", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+            Toast.makeText(applicationContext, posFaculti.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
